e762461903d5495bb3f2aeae10a47e20c2ff8938
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-01-17 09:01:28 +0700

    Content: Make Scaffold page independent

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,23 +1,12 @@
+import 'package:courseapp/widgets/customScaffold.dart';
 import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      extendBodyBehindAppBar: true  ,
-      body: Stack(
-        children: [
-          Image.asset('assets/image/backgroundLanding.png',
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,)
-        ],
-      ),
+    return const Customscaffold(
+      child: Text('Welcom'),
     );
   }
 }
---
+++ lib/widgets/customScaffold.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Customscaffold extends StatelessWidget {
+  const Customscaffold({super.key, this.child});
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Image.asset('assets/image/backgroundLanding.png',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity),
+          SafeArea(child: child!)
+        ],
+      ),
+    );
+  }
+}
